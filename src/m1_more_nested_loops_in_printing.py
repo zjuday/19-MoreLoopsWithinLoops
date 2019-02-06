7eb8b4e914ba807189c2c78a94ce093d175c9264
zjuday <judayzj@rose-hulman.edu>
2019-02-06 10:42:33 -0500

    Not quite done

--- src/m1_more_nested_loops_in_printing.py
+++ src/m1_more_nested_loops_in_printing.py
@@ -4,8 +4,8 @@ in the context of PRINTING on the CONSOLE.
 
 Authors: David Mutchler, Vibha Alangar, Matt Boutell, Dave Fisher,
          Mark Hays, Amanda Stouder, Aaron Wilkin, their colleagues,
-         and PUT_YOUR_NAME_HERE.
-"""  # TODO: 1. PUT YOUR NAME IN THE ABOVE LINE.
+         and Zachary Juday.
+"""  # DONE: 1. PUT YOUR NAME IN THE ABOVE LINE.
 
 
 def main():
@@ -56,7 +56,7 @@ def triangle_right_justified(r):
     For purposes of "lining up", assume r is a single digit.
     """
     # -------------------------------------------------------------------------
-    # TODO: 2. Implement and test this function.
+    # DONE: 2. Implement and test this function.
     #          Some tests are already written for you (above).
     #
     # HINT: Do the following problem FIRST, then convert x's to spaces:
@@ -75,6 +75,13 @@ def triangle_right_justified(r):
     #   defeat the goal of providing practice at loops within loops.
     # -------------------------------------------------------------------------
 
+    for k in range(r):
+        spaces = r-k-1
+        for _ in range(spaces):
+            print(' ',end='')
+        for j in range(k):
+            print(j+1,end='')
+        print(k+1)
 
 def run_test_triangle_upside_down():
     """ Tests the    triangle_upside_down    function. """
@@ -119,6 +126,13 @@ def triangle_upside_down(r):
     #   defeat the goal of providing practice at loops within loops.
     # -------------------------------------------------------------------------
 
+    for k in range(r):
+        total = r - k
+        for _ in range(k):
+            print(' ',end='')
+        for j in range(total):
+            print(j+1,end='')
+        print()
 
 def run_test_vee():
     """ Tests the    vee    function. """
@@ -170,7 +184,17 @@ def vee(r):
     #   defeat the goal of providing practice at loops within loops.
     # -------------------------------------------------------------------------
 
-
+    for k in range(r):
+        total = r - k
+        for _ in range(k):
+            print(' ',end='')
+        for a in range(total):
+            print(a+1,end='')
+        print('-',end='')
+        for j in range(total,0,-1):
+            print(j,end='')
+        print()
+    
 def run_test_numbers_constant_forward():
     """ Tests the    numbers_constant_forward    function. """
     print()
